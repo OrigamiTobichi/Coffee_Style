187ed97b590b0030f91ae46fbbb82eb20bfe458e
OrigamiTobichi <nguyenduc09012003@gmail.com>
2023-01-08 23:07:25 +0700

    update-8/1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,30 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      spacing:{
+        7:'7px',
+      },
+      fontFamily:{
+        Karla: ['Karla', 'sans-serif']
+      },
       colors: {
         "light-coffee": "#C89F94",
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        fadeIn: {
+          'from': {opacity: 0},
+          'to': {opacity: 1},
+          '0%': { transform: 'translate(-50%, 50%)' },
+          '100%': { transform: 'translate(-50%, 0)' },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 0.4s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out',
       },
     },
   },
